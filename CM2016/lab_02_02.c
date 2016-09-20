5c753f9fa9fe445ef2b206999d3dedfba0d9b758
kubasovainna <kubasovainna@mail.ru>
2016-09-20 17:06:45 +0300

    Created by Coding Ground Client

---
+++ CM2016/lab_02_02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main() {
+    int i=4, j=0;
+    for (i=4;i<5;i++) {
+        printf("Цикл for: i= %d\n",i);
+    }
+    i=5;
+        while (j==0 && i<5){
+            printf("Цикл  while: i= %d\n",i);
+            i=i+1;
+        }
+        i=5;
+        do {
+            printf("Цикл do: i= %d\n",i);
+            i++;
+        }   while (i<5);
+        
+        int A=
+}
